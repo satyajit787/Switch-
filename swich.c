5f04e01581295f7342a5c03d2cbd983cdb02f005
satyajit787 <satyajitv069@gmail.com>
2024-07-30 11:52:10 +0530

    Add files via upload

---
+++ swich.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+void main(){
+
+int month;
+
+printf("Entar your month :");
+
+scanf("%d",&month);
+
+switch(month){
+
+case 1:
+    printf("January");
+    break;
+
+case 2:
+    printf("February");
+    break;
+
+case 3:
+    printf("March");
+    break;
+
+case 4:
+    printf("April");
+    break;
+
+case 5:
+    printf("May");
+    break;
+
+case 6:
+    printf("June");
+    break;
+
+case 7:
+    printf("July");
+    break;
+
+case 8:
+    printf("august");
+    break;
+
+case 9:
+    printf("September");
+    break;
+
+case 10:
+    printf("October");
+    break;
+
+case 11:
+    printf("November");
+    break;
+
+case 12:
+    printf("Decenber");
+    break;
+
+default:
+    printf("Month not found");
+
+
+
+}
+
+
+
+}
